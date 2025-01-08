4942a70c8d2cc16b9b5719e36fe81a034a8a6b1c
Omar Quazi <omar.quazi@okta.com>
2025-01-08 13:00:34 -0500

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,24 @@
+var cacheName = "pwa-test-cheches"
+var urlsToCache = [
+	"/localhost:5500/",
+]
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache)
+      })
+    )
+})
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function(response) {
+        return response ? response : fetch(event.request)
+      })
+    )
+})
